3fa37d45d8f98d7425e0537dc939669c4c36bf3c
yuxuanjerrychen01 <yuxuanjerrychen01@163.com>
2025-10-17 08:31:37 +0000

    Deploying to gh-pages from @ yuxuanjerrychen01/yuxuanjerrychen01.github.io@971159ba61ac01c729ce0ac4edff75358a3ee98b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -483,13 +483,6 @@ ninja.data = [{
           window.open("https://orcid.org/0009-0009-2159-8746", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
