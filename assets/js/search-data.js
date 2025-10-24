96a95baaf082e3d3ac00d28aeca30e56024b6990
yuxuanjerrychen01 <yuxuanjerrychen01@163.com>
2025-10-24 05:36:49 +0000

    Deploying to gh-pages from @ yuxuanjerrychen01/yuxuanjerrychen01.github.io@ba1ae8fb7f8ba7a15684c9e299cfaa6722420126 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
